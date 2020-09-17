bca1ff7fd3755ea20ea67a6656de73d2f07de4b7
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-17 14:01:19 -0400

    fix

--- countOnly.js
+++ countOnly.js
@@ -1,36 +1,21 @@
-const eqArrays = function(array1, array2) {
-    let correct = true;
-  
-    //if the two arrays are not the same length the assertion fails automatically
-    if (array1.length !== array2.length) {
-      correct = false;
-    } 
-  
-    //if the two arrays are the same length this runs:
-    for (let i = 0; i < array1.length; i ++) {
-      if (array1[i] !== array2[i]) {
-        correct = false;
-      }
-    }
-  
-    return correct;
-  
-  }
 
-const assertEqual = function(arr1,arr2){
-    const result = eqArrays(arr1,arr2)
-      if (!result){
-          console.log(`❌ ❌ ❌: Uh Oh! FALSE`)
-      }
-      else {
-          console.log(`✅ ✅ ✅ : Congratulations! TRUE`)
-      }
-    }
+const results = 
+    { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false };
+    
+    const firstNames = [
+        "Karl",
+        "Salima",
+        "Agouhanna",
+        "Fang",
+        "Kavith",
+        "Jason",
+        "Salima",
+        "Fang",
+        "Joe"
+      ];
 
-
-
-
-const countOnly = function(allItems, itemsToCount) {
+const countOnly = function(allItems, itemsToCount) 
+{
     const results = {};
 
     for (const item of allItems) {
@@ -40,29 +25,7 @@ const countOnly = function(allItems, itemsToCount) {
     return results;
    
 }
-
-const results = 
-{ "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false }
- return results ;
-
-
-
-const firstNames = [
-    "Karl",
-    "Salima",
-    "Agouhanna",
-    "Fang",
-    "Kavith",
-    "Jason",
-    "Salima",
-    "Fang",
-    "Joe"
-  ];
-
-  
-  
-  
- countOnlytt(firstNames, results );
+ countOnly([firstNames], results );
 
 //console.log(result1["Jason"], 1);
 //result1["Karima"], undefined;
