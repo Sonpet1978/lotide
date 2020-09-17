f8c04ba5987b37d423d1f2205739abb2b608edd3
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-17 10:33:40 -0400

    eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,16 @@
-const assertEqual = function(actual, expected) {
-    assert.equal(actual, expected); 
+const eqArrays = function(array1, array2) {
+    for(var i = 0; i <= array1.length; i++) {
+        if(array1[i] !== array2[i]) {
+            return false;
+        }
+    }
+    return true;
   
 };
 
-// TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// TEST CODE 
+console.log(eqArrays([1, 2, 3], [1, 2, 3])); // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])); // => false
